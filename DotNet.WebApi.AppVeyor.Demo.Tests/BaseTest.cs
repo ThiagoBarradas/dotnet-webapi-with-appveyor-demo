763bce44895f4c423edad92329b4ac9b53b94be5
Thiago Barradas <thiagobrowskt@gmail.com>
2017-02-16 23:57:37 -0200

    :white_check_mark: Implements a simple unit test

---
+++ DotNet.WebApi.AppVeyor.Demo.Tests/BaseTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DotNet.WebApi.AppVeyor.Demo.Controllers;
+using System.Web.Http;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Routing;
+using System.Net;
+
+namespace DotNet.WebApi.AppVeyor.Demo.Tests
+{
+    public class BaseTest
+    {
+        public T GenerateController<T>(HttpRequestMessage request = null) 
+            where T : ApiController, new()
+        {
+            var controller = new T();
+
+            var config = new HttpConfiguration();
+            var route = new HttpRouteData(new HttpRoute());
+            if (request == null) request = new HttpRequestMessage();
+
+            controller.ControllerContext = new HttpControllerContext(config, route, request);
+            controller.Request = request;
+
+            return controller;
+        }
+    }
+}
---
+++ DotNet.WebApi.AppVeyor.Demo.Tests/HomeControllerTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DotNet.WebApi.AppVeyor.Demo.Controllers;
+using System.Web.Http;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Routing;
+using System.Net;
+
+namespace DotNet.WebApi.AppVeyor.Demo.Tests
+{
+    [TestClass]
+    public class HomeControllerTest : BaseTest
+    {       
+        [TestMethod]
+        public void Home_Should_Return_OK()
+        {
+            // Arrange
+            HomeController controller = this.GenerateController<HomeController>();
+            
+            // Act
+            var result = controller.Home();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.StatusCode, HttpStatusCode.OK);
+        }
+    }
+}
